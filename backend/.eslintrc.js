cb08b9e52c3edf434ad82e7e7b77ab2ef639d0f9
Dayvid <ruan12bbg@gmail.com>
2022-05-07 11:42:33 -0300

    config: Instalações iniciais

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "comma-spacing": ["error", {before: false, after: true}],
+  }
+};
