82e341ec41df36ba2b77796b2f07722e4b9eda15
Dayvid <ruan12bbg@gmail.com>
2022-05-09 15:31:51 -0300

    feat: testes de login implementados

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "browser": true,
     "commonjs": true,
     "es2021": true,
+    "mocha": true,
     "node": true
   },
   "extends": "eslint:recommended",
@@ -13,6 +14,6 @@ module.exports = {
     "semi": ["error", "always"],
     "quotes": ["error", "double"],
     "indent": ["error", 2],
-    "comma-spacing": ["error", {before: false, after: true}],
+    "comma-spacing": ["error", { before: false, after: true }],
   }
 };
